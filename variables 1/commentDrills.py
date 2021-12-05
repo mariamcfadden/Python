27a3b78c01ea5ac84f747403fac242ccc7cf886c
mariamcfadden <93632600+mariamcfadden@users.noreply.github.com>
2021-12-04 19:18:14 -0600

    comment drill

---
+++ variables 1/commentDrills.py
@@ -0,0 +1,42 @@
+'''
+For this assignment you should read the task, then below the task do what 
+it asks you to do.
+EXAMPLE TASK:
+'''
+#EX) Make a single line comment
+
+#This is a single line comment
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+#1) Make a single line comment.
+#this is my single line comment
+
+
+#2) Make a multi-line comment.
+'''
+this 
+is my
+multi-line
+comment
+'''
+
+#3) Make a variable. Then put a comment after it, on the same line.
+a = 3 #this is a single line comment
+
+
+#4) Make a single line comment.
+
+#single line comment is made
+
+#5) Make a multi-line comment.
+
+''' 
+this is also
+a multi-lined comment
