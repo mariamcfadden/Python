007b8c439b0cd1a07757f91c84c74de5c512142c
mariamcfadden <93632600+mariamcfadden@users.noreply.github.com>
2021-12-04 19:04:10 -0600

    comments

---
+++ variables 1/commentsOutline.py
@@ -0,0 +1,21 @@
+'''
+This outline will help solidify concepts from the Comments lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make a single line comment.
+#this is a single line comment
+
+
+#2) Make a multi-line comment.
+'''
+this
+is
+a
+multi-line
+comment
+'''
+#3) Make an int variable. Then, make a single line comment on the 
+#same line, after the variable.
+var = 4 #this is the single line comment
+
