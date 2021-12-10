eb2334a57f73a63630bd8c94f9e8a919b4150236
mariamcfadden <93632600+mariamcfadden@users.noreply.github.com>
2021-12-09 19:35:38 -0600

    survey

---
+++ survey/survey.py
@@ -0,0 +1,19 @@
+'''
+Created on Dec 9, 2021
+
+@author: ITAUser
+'''
+name = input("What is your name?")
+print(name)
+
+favColor = input("What is your favorite color?")
+print(favColor)
+
+city = input("What city did you grow up in?")
+print(city)
+
+friend = input("What is your best friends name?")
+print(friend)
+
+hair = input("What color hair do you have?")
+print(hair)
--- variables 1/inputDrills.py
+++ variables 1/inputDrills.py
@@ -7,7 +7,7 @@ EXAMPLE TASK:
 #EX) Get input from the user to the question "What is your favorite color?"
 #and store their answer in a variable called favColor.
 favColor = input("What is your favorite color?")
-
+print(favColor)
 '''
 END OF EXAMPLE
 '''
@@ -19,25 +19,30 @@ START HERE
 #1) Get input from the user to the question "What is your favorite animal?"
 #and store their answer in a variable called favAnimal.
 
-
+favAnimal = input("what is your favorite animal")
+print(favAnimal)
 
 #2) Get input from the user to the question "What is your favorite number?"
 #and store their answer in a variable called favNum. Convert favNum from a 
 #string variable to a int variable.
 
-
-
+favNum = input("what is your favorite number?")
+age=int(input("what is your age?"))
+print(age)
 #3) Get input from the user to the question "What is your school's name?"
 #and store their answer in a variable called school.
-
-
+school = input("what is your school's name?")
+school=int(school)
+school=int(input("what is your school's name?"))
 #4) Get input from the user to the question "What is your instructors name?"
 #and store their answer in a variable called instructor.
-
-
-
+instuctor = input ("what is your instructors name?")
+instructor=int(instructor)
+instructor=int(input("what is your instructors name?"))
 #5) Get input from the user to the question "What is 2 + 2?"
 #and store their answer in a variable called ans. Convert ans from a string
 #variable to a int variable
-
+ans = input
+ans=int(2 +2)
+ans=int(input("what is 2 + 2?"))
 
