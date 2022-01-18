72a34b49dd74a0321a366e4f2be05eeb1ea56e51
mariamcfadden <93632600+mariamcfadden@users.noreply.github.com>
2022-01-18 14:20:44 -0600

    input

---
+++ Input/mathOutline.py
@@ -0,0 +1,74 @@
+'''
+This outline will help solidify concepts from the Mathematical Operators lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make two string variables and use the + operator to combine them into
+#one new variable.
+stringVar = " Hello World! "
+print ("this is a string:" + stringVar)
+
+
+#2) Make two int variables and use the + operator to combine them into
+#one new variable.
+x = 4 + 4
+print(x)
+
+
+#3) Make two int variables and use the - operator to combine them into
+#one new variable.
+x = 5 - 1
+print(x)
+
+
+#4) Make two float variables and use the - operator to combine them into
+#one new variable.
+x = 3.33 - 1.33
+print(x)
+
+#5) Make two int variables and use the * operator to combine them into
+#one new variable.
+x = 5 * 2
+print(x)
+
+
+#6) Make one string variable and one int variable and use the * operator 
+#to combine them into one new variable.
+stringVar = " Hello * World"
+print ("this is a string:" + stringVar)
+
+
+#7) Make two int variables and use the / operator to combine them into
+#one new variable.
+x = 16 / 4
+print(x)
+
+
+#8) Make two float variables and use the / operator to combine them into
+#one new variable.
+x = 17.5 / 3
+print(x)
+
+
+#9) Make two int variables and use the % operator to combine them into
+#one new variable.
+
+x = 6 % 3
+print(x)
+
+#10) Make two int variables and use the // operator to combine them into
+#one new variable.
+
+x = 5 // 5
+print(x)
+
+#11) Make two int variables and use the ** operator to combine them into
+#one new variable.
+x = 5 ** 2
+print(x)
+
+
+#12) Make any type of operation with a common SYNTAX error
+
+x = "hello world"
+print(x)
---
+++ variables 1/inputOutline.py
@@ -0,0 +1,18 @@
+#Use input() to ask the user "What is your name?" and store that in
+#a variable called name. Then print the variable.
+
+
+#Use input() to ask the user "What is your favorite color?" and store that 
+#in a variable called color. Then print the variable.
+
+
+#Use input() to ask the user "What city did you grow up in?" and store 
+#that in a variable called city. Then print the variable.
+
+
+#Use input() to ask the user "What is your best friends name?" and store 
+#that in a variable called friend. Then print the variable.
+
+
+#Use input() to ask the user "What color hair do you have?" and store that 
+#in a variable called hair. Then print the variable.
