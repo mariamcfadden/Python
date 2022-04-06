5c5e01368db8152477d83e8faf4ab17515c7b855
mariamcfadden <93632600+mariamcfadden@users.noreply.github.com>
2022-04-05 22:26:18 -0500

    debug functions

--- functions/functionOutline.py
+++ functions/functionOutline.py
@@ -6,18 +6,32 @@ Fill in this outline as the instructor goes through the lesson.
 #1) Make a function that has two boolean parameters. If both booleans are
 #true, return true. Else, return false. Then, call the function.
 #Print what the function returns.
+def sumTwoNum(numOne, numTwo):
+    answer = numOne + numTwo
+    return answer
 
+x = 2
+y = 3
+sumTwoNum(x,y)
 
+print(sumTwoNum(x,y))
 
 #2) Make a function that takes one int parameter: gallons. Convert gallons
 #to cups (do this by multiplying gallons by 16). Then return cups. Then,
 #call the function.
 #Print what the function returns.
-
+def function(gallons, cups):
+    answer = gallons + cups
+    return answer
+    
+    
+    
 
 
 #3) Make a function of any any kind with a common SYNTAX error. Then, call the
 #function.
 #Print what the function returns.
-
+def function(Maria, McFadden):
+    answer = Maria + McFadden
+    return answer
 
